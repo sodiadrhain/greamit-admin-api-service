2cf1fc179a5bb7d828d8e9b40f569c3929f32edb
sodiadrhain <adesojiawobajo@gmail.com>
2020-11-15 15:57:56 +0100

    modified

--- app/config/database.js
+++ app/config/database.js
@@ -1,16 +1,18 @@
 'use strict'
-const config = {
-    "type": process.env.FB_TYPE,
-    "project_id": process.env.FB_PROJECT_ID,
-    "private_key_id": process.env.FB_PRIVATE_KEY_ID,
-    "private_key": process.env.FB_PRIVATE_KEY,
-    "client_email": process.env.FB_CLIENT_EMAIL,
-    "client_id": process.env.FB_CLIENT_ID,
-    "auth_uri": process.env.FB_AUTH_URL,
-    "token_uri": process.env.FB_TOKEN_URL,
-    "auth_provider_x509_cert_url": process.env.FB_AUTH_PROVIDER,
-    "client_x509_cert_url": process.env.FB_CLIENT_URL
-  }
+// const config = {
+//     "type": process.env.FB_TYPE,
+//     "project_id": process.env.FB_PROJECT_ID,
+//     "private_key_id": process.env.FB_PRIVATE_KEY_ID,
+//     "private_key": process.env.FB_PRIVATE_KEY,
+//     "client_email": process.env.FB_CLIENT_EMAIL,
+//     "client_id": process.env.FB_CLIENT_ID,
+//     "auth_uri": process.env.FB_AUTH_URL,
+//     "token_uri": process.env.FB_TOKEN_URL,
+//     "auth_provider_x509_cert_url": process.env.FB_AUTH_PROVIDER,
+//     "client_x509_cert_url": process.env.FB_CLIENT_URL
+//   }
+
+const config = require('./greamit-693db-firebase-adminsdk-zm6dh-f1a2a71324.json');
 
 module.exports = {
     firebaseConfig: config
